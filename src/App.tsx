1c3240214c042a891903b40fb7424e44f2f4f0d4
toufiq007 <t.limon2342@gmail.com>
2024-05-02 23:21:45 +0600

    create total separate counterContainer

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,8 @@
-import { useState } from "react";
-import "./App.css";
-import Counter from "./components/counter/Counter";
-import Stat from "./components/counter/Stat";
-
-const initialState = [
-  {
-    id: 1,
-    count: 2,
-  },
-  {
-    id: 2,
-    count: 3,
-  },
-];
 
 function App() {
-  const [state, setState] = useState(initialState);
-  const totalCount = state.reduce(
-    (accumulator, currentValue) => accumulator + currentValue.count,
-    0
-  );
-  const handleIncrement = (id: number) => {
-    const updateState = state.map((singleState) => {
-      if (singleState.id === id) {
-        return {
-          ...singleState,
-          count: singleState.count + 1,
-        };
-      }
-      return {
-        ...singleState,
-      };
-    });
-    setState(updateState);
-  };
-  const handleDecrement = (id: number) => {
-    const updateState = state.map((singleState) => {
-      if (singleState.id === id) {
-        return {
-          ...singleState,
-          count: singleState.count - 1,
-        };
-      }
-      return { ...singleState };
-    });
-    setState(updateState);
-  };
-
-  console.log(totalCount);
-
   return (
     <>
-      <h2 className="text-4xl text-center my-3">Simple Counter App</h2>
-      {state.map((singleState, index) => {
-        return (
-          <Counter
-            key={index}
-            id={singleState.id}
-            handleIncrement={handleIncrement}
-            handleDecrement={handleDecrement}
-            count={singleState.count}
-          />
-        );
-      })}
-      <Stat totalCount={totalCount} />
+      
     </>
   );
 }
---
+++ src/components/counter/CounterContainer.tsx
@@ -0,0 +1,70 @@
+import { useState } from "react";
+import Counter from "./Counter";
+import Stat from "./Stat";
+
+const initialState = [
+  {
+    id: 1,
+    count: 2,
+  },
+  {
+    id: 2,
+    count: 3,
+  },
+];
+
+const CounterContainer = () => {
+    const [state, setState] = useState(initialState);
+  const totalCount = state.reduce(
+    (accumulator, currentValue) => accumulator + currentValue.count,
+    0
+  );
+  const handleIncrement = (id: number) => {
+    const updateState = state.map((singleState) => {
+      if (singleState.id === id) {
+        return {
+          ...singleState,
+          count: singleState.count + 1,
+        };
+      }
+      return {
+        ...singleState,
+      };
+    });
+    setState(updateState);
+  };
+  const handleDecrement = (id: number) => {
+    const updateState = state.map((singleState) => {
+      if (singleState.id === id) {
+        return {
+          ...singleState,
+          count: singleState.count - 1,
+        };
+      }
+      return { ...singleState };
+    });
+    setState(updateState);
+  };
+
+  console.log(totalCount);
+
+  return (
+    <>
+      <h2 className="text-4xl text-center my-3">Simple Counter App</h2>
+      {state.map((singleState, index) => {
+        return (
+          <Counter
+            key={index}
+            id={singleState.id}
+            handleIncrement={handleIncrement}
+            handleDecrement={handleDecrement}
+            count={singleState.count}
+          />
+        );
+      })}
+      <Stat totalCount={totalCount} />
+    </>
+  );
+};
+
+export default CounterContainer;
