3e1a0f9fc395a2d6231110190c42fcd3c6a905cf
toufiq007 <t.limon2342@gmail.com>
2024-05-10 07:53:54 +0600

    apply devtools to our project

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,7 +2,11 @@ import { createStore, applyMiddleware } from "redux";
 import { rootReducer } from "./rootReducer";
 import { myLogger } from "./middlewares/customLogger";
 import logger from "redux-logger";
+import { composeWithDevTools } from '@redux-devtools/extension';
 
-const store = createStore(rootReducer, applyMiddleware(myLogger, logger));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(myLogger, logger))
+);
 
 export default store;
